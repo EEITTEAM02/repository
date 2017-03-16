dc5e2ccaa04bb0ac65a4c89a5c2e3ece32877931
林琪家 <vu0ru043@yct>
2017-03-16 14:15:23 +0800

    第3次commit
    增加一個java檔

---
+++ LabWebServletJdbcMaven/src/main/java/model/Model.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Model {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Commit策是修改");
+    System.out.println("增加一姑java檔");
+    System.out.println("hahahahahahaha");
+	}
+
+}
